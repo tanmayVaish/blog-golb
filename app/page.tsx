7c90a8279c1ba1ca45a23784d25a7895078e92ca
tanmayVaish <tanmay777leon@gmail.com>
2023-05-20 22:46:55 +0530

    navbar and blog page added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+import BlogHome from './components/BlogHome';
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <BlogHome />
+    </div>
+  );
+}
